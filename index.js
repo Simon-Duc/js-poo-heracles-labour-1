ce625c3736a8ae8c0d47d583975b958735eea25b
Simon Duc <ducsimon@gmail.com>
2022-03-24 11:43:12 +0100

    almost finished the project, except : styling the strings, rectifying the negative results

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 // First Labour : Heracles vs Nemean Lion
 // use your Figher class here
+
+const fighterClass = require("./src/Fighter");
+
+const heracles = new fighterClass("Heracles", 20, 6);
+
+const nemeanLion = new fighterClass("Nemean Lion", 11, 13);
+
+function thereCanBeOnlyOne(fighter1, fighter2) {
+    for (let round = 1; fighter1.life > 0 && fighter2.life > 0; round++) {
+        let fighter1Attack = fighter1.fight(fighter1, fighter2);
+        let fighter2Attack = fighter2.fight(fighter2, fighter1);
+        let roundResults = `After round ${round}, ${fighter1.name} attacked ${fighter2.name} who has ${fighter2.life} life points remaining. Then ${fighter2.name} attacked ${fighter1.name} who has ${fighter1.life} life points remaining.`;
+        console.log(roundResults);
+    }
+    if (fighter1.life === 0) {
+        console.log(`${fighter2.name} has defeated ${fighter1.name}`);
+    }
+    else {
+        console.log(`${fighter1.name} has defeated ${fighter2.name}`);
+    }
+}
+
+thereCanBeOnlyOne(heracles, nemeanLion);
--- src/Fighter.js
+++ src/Fighter.js
@@ -1,1 +1,27 @@
 /* Fighter class definition */
+const MAX_LIFE = 100;
+
+class Fighter {
+    constructor(name, strength, dexterity, life) {
+        this.name = name;
+        this.strength = strength;
+        this.dexterity = dexterity;
+        this.life = MAX_LIFE;
+    }
+    fight(fighter1, fighter2) {
+        const attack = Math.floor(Math.random() * fighter1.strength);
+        const defense = Math.floor(Math.random() * fighter2.dexterity);;
+        if (attack - defense <= 0) {
+            return fighter2.life 
+        }
+        else if ((fighter2.life -= (attack - defense)) <= 0) {
+            return 0;           
+        }
+        else {
+            return fighter2.life -= (attack - defense);
+        }
+    }
+}
+
+
+module.exports = Fighter;
